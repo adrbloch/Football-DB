0a91dfab98ed162693bae6c90347784e6b106c8e
adrbloch <adrbloch@wp.pl>
2020-10-02 15:15:50 +0200

    Initial commit

---
+++ src/main/java/io/github/adrbloch/FootballDB/FootballDbApplication.java
@@ -0,0 +1,13 @@
+package io.github.adrbloch.FootballDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/adrbloch/FootballDB/FootballDbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adrbloch.FootballDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
