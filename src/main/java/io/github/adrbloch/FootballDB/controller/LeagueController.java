1dae73e9fa582351e3d64030a5c479f392a6a0ca
adrbloch <adrbloch@wp.pl>
2020-10-30 13:35:57 +0100

    Extend League Controller, connect with frontend
    Finish League frontend page handling

--- src/main/java/io/github/adrbloch/FootballDB/controller/LeagueController.java
+++ src/main/java/io/github/adrbloch/FootballDB/controller/LeagueController.java
@@ -2,28 +2,32 @@ package io.github.adrbloch.FootballDB.controller;
 
 import io.github.adrbloch.FootballDB.model.league.League;
 import io.github.adrbloch.FootballDB.service.LeagueService;
+import io.github.adrbloch.FootballDB.service.TableService;
+import io.github.adrbloch.FootballDB.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/league")
 public class LeagueController {
 
     private final LeagueService leagueService;
+    private final TeamService teamService;
+    private final TableService tableService;
+
 
     @Autowired
-    public LeagueController(LeagueService leagueService) {
+    public LeagueController(LeagueService leagueService, TeamService teamService, TableService tableService) {
         this.leagueService = leagueService;
+        this.teamService = teamService;
+        this.tableService = tableService;
     }
 
 
     @PostMapping("/results/byCountry")
-    public String viewAllByCountry (@ModelAttribute("league") League league, Model model) {
+    public String viewAllByCountry(@ModelAttribute("league") League league, Model model) {
 
         model.addAttribute("leagues", leagueService
                 .findLeaguesByCountry(league.getStrCountry())
@@ -47,8 +51,29 @@ public class LeagueController {
     }
 
     @GetMapping("/search")
-    public String searchByName(Model model) {
+    public String searchLeagues(Model model) {
         model.addAttribute("league", new League());
         return "search/searchLeagues";
+    }
+
+    @GetMapping("/{id}")
+    public String viewLeagueDetails(@PathVariable("id") String id, Model model) {
+        League league = leagueService
+                .findLeagueById(id)
+                .block()
+                .getLeagues()
+                .get(0);
+
+        model.addAttribute("league", league);
+
+        model.addAttribute("teamsByLeague", teamService.findTeamsByLeague(league
+                .getStrLeague())
+                .block()
+                .getTeams());
+
+        model.addAttribute("tableByLeague", tableService.findTableByLeagueIdAndSeason(league.getIdLeague(),
+                league.getStrCurrentSeason()));
+
+        return "data/league";
     }
 }
